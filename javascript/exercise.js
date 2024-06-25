fbb0c5d827f2f0291cb6819510ff95ba1410c5c0
ganesh200328 <ganesh200328@gmail.com>
2024-06-25 12:33:50 +0530

    manipulating jsexercise

--- javascript/exercise.js
+++ javascript/exercise.js
@@ -1,15 +1,19 @@
 //if else exercise
 function nouse()
 {
-    var answer="ye"
+    var answer="yes"
     if (answer=="yes"){
+        var a =10;
         console.log("Your teeth are clean")
     }
+    
     else
     {
         console.log("You need to clean your teeth")
     }
+    console.log("if  "+a)
     
 }
-answe =nouse()
-console.log("let variable"+answe)
+nouse()
+//answe =nouse()
+//console.log("let variable"+answe)
